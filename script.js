53e34b092dd634024c95f4d6beea28bb5b350fa7
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-09-23 15:40:51 -0300

    Resumo

---
+++ script.js
@@ -0,0 +1,54 @@
+        // let, var, const -> tipagem dinâmica
+
+        // *const const_var = 10//const não altera o valor nunca.
+
+        // let valor = 10;// Tipagem dinamica do JS
+        // console.log(typeof(valor),valor)
+
+        // valor = "banana"
+        // console.log(typeof(valor),valor)
+
+
+        // let x = "Teste Let" //escopo local
+        // var y = "Teste Var" // escopo global
+
+        // function TesteLet(){
+        //     if(true){
+        //     let x = 10
+        //         }
+        //         console.log(x)
+        // }
+
+        // function TesteVar(){
+        //     if(true){
+        //         var y = "Teste do var"
+        //     }
+        //     console.log(y)
+        // }
+
+        // TesteLet();
+        // TesteVar();
+        
+        // function FuncComParams(x, y , z){
+
+
+
+        // }
+        
+        // let carro = {
+        //     "Marca": "Bugatti",
+        //     "Modelo": "Chiron",
+        //     "Cor": "Azul",
+        //     "Propietario": {
+        //         "nome": "Neymar",
+        //         "sobrenome": "Jr"
+
+        //     }
+
+        // }
+        // console.log(carro)
+
+        let arr = [1, 2, 3, 4, "Henrique", true, false, undefined, null]
+        let mtx = [[123], [123], [123]]
+        console.log(mtx)
+        
